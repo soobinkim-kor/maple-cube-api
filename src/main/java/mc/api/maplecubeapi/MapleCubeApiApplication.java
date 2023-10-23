7b6f396298d6ec4c38d398e05f8a8ba756e5b5ba
soobinkim-kor <shb03207@naver.com>
2023-10-23 23:19:47 +0900

    Initial commit

---
+++ src/main/java/mc/api/maplecubeapi/MapleCubeApiApplication.java
@@ -0,0 +1,13 @@
+package mc.api.maplecubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleCubeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleCubeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mc/api/maplecubeapi/MapleCubeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mc.api.maplecubeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleCubeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
